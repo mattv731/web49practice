389a00656d8b40c94b14b890dad14adc4265aab5
abc <abc@example.com>
2022-01-13 11:02:24 -0700

    added .env to get ignore

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const user = process.env.USERNAME
-const cohort = process.argv[2]
-
-if (process.argv[2] === 'web-49'){
-    console.log(`welcome ${user} to ${cohort}`)
-} else {
-    console.log(`Boooooo ${cohort}`)
-}
 
 const express = require('express')
 const app = express()
@@ -14,7 +6,7 @@ app.get('/hello', (req, res) => {
     res.json({message: "hey you person"})
 })
 
-const port = 9000
+const port = process.env.PORT || 9000 // heroku wants to set it's own port
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
