b49faafc48a40e171d228e8027d236d8ae65b805
abc <abc@example.com>
2022-01-13 10:51:42 -0700

    added to index.js

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ if (process.argv[2] === 'web-49'){
 } else {
     console.log(`Boooooo ${cohort}`)
 }
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.json({message: "hey you person"})
+})
+
+const port = 9000
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
